82dc2ec1dda244daf0f7b8780c57262829f0fb42
Tim Geyssens <tg@umbraco.com>
2013-06-24 12:28:50 +0200

    CoffeeScript provider WIP

--- BundlingAndMinificationForTheMasses.Providers.Coffee/Translator.cs
+++ BundlingAndMinificationForTheMasses.Providers.Coffee/Translator.cs
@@ -1,11 +1,78 @@
-﻿using System;
+﻿using BundleTransformer.CoffeeScript.Translators;
+using BundleTransformer.Core.Assets;
+using BundleTransformer.Core.Translators;
+using BundlingAndMinificationForTheMasses.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
 namespace BundlingAndMinificationForTheMasses.Providers.CoffeeScript
 {
-    public class Translator
+    public class Translator : IFileTranslator
     {
+        public string Name
+        {
+            get { return "CoffeeScript"; }
+        }
+
+        public string FileExtension
+        {
+            get { return "coffee"; }
+        }
+
+        public string FileMimeType
+        {
+            get { return "text/x-coffeescript"; }
+        }
+
+        public Enums.TranslatorType TranslatorType
+        {
+            get { return Enums.TranslatorType.Script; }
+        }
+
+        public IEnumerable<string> EditorClientDependencies
+        {
+            get
+            {
+                var scripts = new List<string>();
+                scripts.Add("CodeMirror/js/mode/Coffeescript/coffeescript");
+                return scripts;
+            }
+        }
+
+        public IEnumerable<Exception> ValidateTranslation(string path)
+        {
+            var retVal = new List<Exception>();
+            try
+            {
+                IAsset file = new Asset(path);
+                CoffeeScriptTranslator translator = new CoffeeScriptTranslator();
+                translator.Translate(file);
+            }
+            catch (AssetTranslationException ex)
+            {
+                retVal.Add(ex);
+            }
+
+            return retVal;
+
+        }
+
+        public void SaveTranslation(string path)
+        {
+            IAsset file = new Asset(path);
+            CoffeeScriptTranslator translator = new CoffeeScriptTranslator();
+            var compiled = translator.Translate(file);
+
+            var normalScript = path.Replace("." + FileExtension, ".js");
+
+            using (var compiledScript = File.CreateText(normalScript))
+            {
+                compiledScript.Write(compiled.Content);
+                compiledScript.Close();
+            }
+        }
     }
 }
