d40d74fea0b6497113f1b12d079e9c62e58cd1b7
Tim Geyssens <tg@umbraco.com>
2013-06-26 19:25:11 +0200

    Scripts tree

--- BundlingAndMinificationForTheMasses/Umbraco/Trees/DynamicScriptTree.cs
+++ BundlingAndMinificationForTheMasses/Umbraco/Trees/DynamicScriptTree.cs
@@ -1,11 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
+using Umbraco.Core.IO;
+using umbraco.BusinessLogic.Actions;
+using umbraco.businesslogic;
+using umbraco.cms.presentation.Trees;
+using umbraco.interfaces;
+using umbraco.cms.businesslogic.translation;
 
-namespace BundlingAndMinificationForTheMasses.Umbraco.Trees
+namespace Optimus.Umbraco.Trees
 {
-    public class DynamicScriptTree
+    [Tree("settings", "scriptsNew", "Dynamic Scripts")]
+    public class DynamicScriptTree : BaseTree
     {
+        public DynamicScriptTree(string application)
+            : base(application)
+        {
+        }
+
+        public override void RenderJS(ref StringBuilder Javascript)
+        {
+            Javascript.Append(
+            @"
+                function openDyanmicScriptFileEditor(fileName, compiled) {
+                    UmbClientMgr.contentFrame('../App_Plugins/Optimus/Pages/FileEditor.aspx?file='+ fileName + '&path=/scripts/&compiled=' + compiled);
+                }");
+        }
+        
+
+        public override void Render(ref XmlTree tree)
+        {
+            string orgPath  = string.Empty;
+            string path     = string.Empty;
+            string FilePath = "/scripts/";
+
+            if (!string.IsNullOrEmpty(this.NodeKey))
+            {
+                orgPath = this.NodeKey;
+                path = IOHelper.MapPath(FilePath + orgPath);
+                orgPath += "/";
+            }
+            else
+            {
+                path = IOHelper.MapPath(FilePath);
+            }
+
+            DirectoryInfo dirInfo       = new DirectoryInfo(path);
+            DirectoryInfo[] dirInfos    = dirInfo.GetDirectories();
+
+            var args = new TreeEventArgs(tree);
+            OnBeforeTreeRender(dirInfo, args);
+
+          
+
+            //Loop through files
+            var fileInfo = dirInfo.GetFilesByExtensions(new Translation.Core().GetPossibleExtensions(Enums.TranslatorType.Script).ToArray());
+
+            foreach (FileInfo file in fileInfo)
+            {
+                if ((file.Attributes & FileAttributes.Hidden) == 0)
+                {
+                    XmlTreeNode xFileNode   = XmlTreeNode.Create(this);
+                    xFileNode.NodeID        = orgPath + file.Name;
+                    xFileNode.Text          = file.Name;
+                    xFileNode.OpenIcon      = "doc.gif";
+                    xFileNode.Menu          = new List<IAction> { ActionDelete.Instance };
+
+                    xFileNode.Icon = new Optimus.Translation.Core().GetTranslatorTreeIconPath(file.Name);
+
+                    
+
+                    //JS Action link...
+                    //Only run/set an action if it's empty (as in not been set above as static/compiled file)
+                    if (string.IsNullOrEmpty(xFileNode.Action))
+                    {
+                        if (orgPath != string.Empty)
+                        {
+                            xFileNode.Action = "javascript:openDyanmicScriptFileEditor('" + orgPath + file.Name + "', false');";
+                        }
+                        else
+                        {
+                            xFileNode.Action = "javascript:openDyanmicScriptFileEditor('" + file.Name + "', 'false');";
+                        }
+                    }
+                    
+
+                    //OnRenderFileNode(ref xFileNode);
+                    OnBeforeNodeRender(ref tree, ref xFileNode, EventArgs.Empty);
+
+                    if (xFileNode != null)
+                    {
+                        tree.Add(xFileNode);
+                        OnAfterNodeRender(ref tree, ref xFileNode, EventArgs.Empty);
+                    }
+                }
+            }
+
+            //After TREE Rendering
+            OnAfterTreeRender(dirInfo, args);
+        }
+
+
+        protected override void CreateRootNode(ref XmlTreeNode rootNode)
+        {
+            rootNode.Icon       = ".sprTreeFolder";
+            rootNode.OpenIcon   = ".sprTreeFolder_o";
+            rootNode.NodeID     = "init";
+            rootNode.NodeType   = rootNode.NodeID + TreeAlias;
+            rootNode.Menu       = new List<IAction> { ActionNew.Instance, ActionRefresh.Instance };
+        }
     }
 }
