8e69d03c99768bc71f0262915ad32fee2dbfe383
Tim Geyssens <tg@umbraco.com>
2013-07-11 17:01:35 +0200

    Now able to bundle sass (or other file) directly, so that don't have to edit in UI (needs config update to make it possible)

---
+++ BundlingAndMinificationForTheMasses/CacheService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Caching;
+
+using umbCache = umbraco.cms.businesslogic.cache.Cache;
+using System.Web;
+
+
+
+namespace Optimus
+{
+    public class CacheService
+    {
+        /// <summary>
+        /// Clears everything in umbraco's runtime cache, which means that not only
+        /// umbraco content is removed, but also other cache items from pages running in
+        /// the same application / website. Use with care :-)
+        /// </summary>
+        public static void ClearAllCache()
+        {
+            umbCache.ClearAllCache();
+        }
+
+        /// <summary>
+        /// Clears the item in umbraco's runtime cache with the given key 
+        /// </summary>
+        /// <param name="Key">Key</param>
+        public static void ClearCacheItem(string Key)
+        {
+            //LogHelper.Debug("clearing log item: " + Key);
+            umbCache.ClearCacheItem(Key);
+        }
+
+
+        /// <summary>
+        /// Clears all objects in the System.Web.Cache with the System.Type name as the
+        /// input parameter. (using [object].GetType())
+        /// </summary>
+        /// <param name="TypeName">The name of the System.Type which should be cleared from cache ex "System.Xml.XmlDocument"</param>
+        public static void ClearCacheObjectTypes(string TypeName)
+        {
+            umbCache.ClearCacheObjectTypes(TypeName);
+        }
+
+        /// <summary>
+        /// Clears all cache items that starts with the key passed.
+        /// </summary>
+        /// <param name="KeyStartsWith">The start of the key</param>
+        public static void ClearCacheByKeySearch(string KeyStartsWith)
+        {
+            umbCache.ClearCacheByKeySearch(KeyStartsWith);
+        }
+
+        /// <summary>
+        /// Retrieve all cached items
+        /// </summary>
+        /// <returns>A hastable containing all cacheitems</returns>
+        public static System.Collections.Hashtable ReturnCacheItemsOrdred()
+        {
+            return umbCache.ReturnCacheItemsOrdred();
+        }
+
+
+        public static TT GetCacheItem<TT>(string cacheKey, object syncLock,
+            TimeSpan timeout, umbCache.GetCacheItemDelegate<TT> getCacheItem)
+        {
+
+            return umbCache.GetCacheItem(cacheKey, syncLock, null, timeout, getCacheItem);
+        }
+
+        public static TT GetCacheItem<TT>(string cacheKey, object syncLock,
+            CacheItemRemovedCallback refreshAction, TimeSpan timeout,
+            umbCache.GetCacheItemDelegate<TT> getCacheItem)
+        {
+            return umbCache.GetCacheItem(cacheKey, syncLock, CacheItemPriority.Normal, refreshAction, timeout, getCacheItem);
+        }
+
+        public static TT GetCacheItem<TT>(string cacheKey, object syncLock,
+            CacheItemPriority priority, CacheItemRemovedCallback refreshAction, TimeSpan timeout,
+            umbCache.GetCacheItemDelegate<TT> getCacheItem)
+        {
+            return umbCache.GetCacheItem(cacheKey, syncLock, priority, refreshAction, null, timeout, getCacheItem);
+        }
+
+        public static TT GetCacheItem<TT>(string cacheKey, object syncLock,
+            CacheItemPriority priority, CacheItemRemovedCallback refreshAction,
+            CacheDependency cacheDependency, TimeSpan timeout, umbCache.GetCacheItemDelegate<TT> getCacheItem)
+        {
+
+            return (TT)umbCache.GetCacheItem<TT>(cacheKey, syncLock, priority, refreshAction, cacheDependency, timeout, getCacheItem);
+        }
+    }
+}
---
+++ BundlingAndMinificationForTheMasses/Settings.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+using System.Xml;
+using umbraco.BusinessLogic;
+
+namespace Optimus
+{
+    public class Settings
+    {
+        public static XmlDocument OptimusConfig
+        {
+            get
+            {
+                XmlDocument us = (XmlDocument)HttpRuntime.Cache["optimusSettingsFile"];
+                if (us == null)
+                    us = ensureSettingsDocument();
+                return us;
+            }
+        }
+
+        private static XmlDocument ensureSettingsDocument()
+        {
+            object settingsFile = HttpRuntime.Cache["optimusSettingsFile"];
+            string fullPath = HostingEnvironment.MapPath(Config.BundlesConfigPath);
+
+            // Check for language file in cache
+            if (settingsFile == null)
+            {
+                XmlDocument temp = new XmlDocument();
+                XmlTextReader settingsReader = new XmlTextReader(fullPath);
+                try
+                {
+                    temp.Load(settingsReader);
+                    HttpRuntime.Cache.Insert("optimusSettingsFile", temp, new CacheDependency(fullPath));
+                }
+                catch (XmlException e)
+                {
+                    throw new XmlException("Your bundles.config file fails to pass as valid XML. Refer to the InnerException for more information", e);
+                }
+                catch (Exception e)
+                {
+                    Log.Add(LogTypes.Error, new User(0), -1, "Error reading bundles.config file: " + e.ToString());
+                }
+                settingsReader.Close();
+                return temp;
+            }
+            else
+                return (XmlDocument)settingsFile;
+        }
+        private static object configCacheSyncLock = new object();
+
+        public static string GetSetting(string key)
+        {
+            return CacheService.GetCacheItem<string>("OptimusKey" + key, configCacheSyncLock, TimeSpan.FromHours(6),
+                    delegate
+                    {
+
+                        XmlNode x = OptimusConfig.SelectSingleNode(string.Format("//setting [@key = '{0}']", key));
+                        if (x != null)
+                            return x.Attributes["value"].Value;
+                        else
+                            return string.Empty;
+                    });
+        }
+    }
+}
