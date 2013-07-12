1a6b7bf07946ca242ff2b2d5d9f58a2311e83bd1
Tim Geyssens <tg@umbraco.com>
2013-07-12 10:00:26 +0200

    Adds new package action

---
+++ BundlingAndMinificationForTheMasses/Helpers/XmlHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Optimus.Helpers
+{
+   public class XmlHelper
+    {
+        /// <summary>
+        /// Gets the value from an attribute or returns an empty string if it wasn't specified
+        /// </summary>
+        public static string GetAttributeValueFromNode(XmlNode node, string attributeName)
+        {
+            return GetAttributeValueFromNode<string>(node, attributeName, string.Empty);
+        }
+
+        /// <summary>
+        /// Gets the value from an attribute or returns defaultValue if it wasn't specified
+        /// </summary>
+        public static string GetAttributeValueFromNode(XmlNode node, string attributeName, string defaultValue)
+        {
+            return GetAttributeValueFromNode<string>(node, attributeName, defaultValue);
+        }
+
+        /// <summary>
+        /// Gets the value from an attribute, if no value or empty, it returns your default value (everything converted to the right type).
+        /// </summary>
+        public static T GetAttributeValueFromNode<T>(XmlNode node, string attributeName, T defaultValue)
+        {
+            if (node.Attributes[attributeName] != null)
+            {
+                string result = node.Attributes[attributeName].InnerText;
+                if (string.IsNullOrEmpty(result))
+                    return defaultValue;
+
+                return (T)Convert.ChangeType(result, typeof(T));
+            }
+            return defaultValue;
+        }
+
+
+    }
+}
--- BundlingAndMinificationForTheMasses/Umbraco/Installer/PackageActions.cs
+++ BundlingAndMinificationForTheMasses/Umbraco/Installer/PackageActions.cs
@@ -312,4 +312,75 @@ namespace Optimus.Umbraco.Installer
             return umbraco.cms.businesslogic.packager.standardPackageActions.helper.parseStringToXmlNode(sample);
         }
     }
+
+    public class SetAttributeValue : IPackageAction
+    {
+        public string Alias()
+        {
+            return "Umbundle.SetAttributeValue";
+        }
+
+        public bool Execute(string packageName, System.Xml.XmlNode xmlData)
+        {
+            //Set result default to false
+            bool result = false;
+
+
+            //The config file we want to modify
+            string configFileName = VirtualPathUtility.ToAbsolute(Helpers.XmlHelper.GetAttributeValueFromNode(xmlData, "file"));
+
+            //Xpath expression to determine the rootnode
+            string xPath = Helpers.XmlHelper.GetAttributeValueFromNode(xmlData, "xpath");
+
+            //Xpath expression to determine the attributeName we want to select
+            string attributeName = Helpers.XmlHelper.GetAttributeValueFromNode(xmlData, "attributeName");
+
+            //Xpath expression to determine the attributeValue we want to select
+            string attributeValue = Helpers.XmlHelper.GetAttributeValueFromNode(xmlData, "attributeValue");
+
+            //Open the config file
+            XmlDocument configDocument = umbraco.xmlHelper.OpenAsXmlDocument(configFileName);
+
+            //Select rootnode using the xpath
+            XmlNode rootNode = configDocument.SelectSingleNode(xPath);
+
+            //If the rootnode != null continue
+            if (rootNode != null)
+            {
+                if (rootNode.Attributes[attributeName] == null)
+                {
+                    //Attribute doesn't exists, create it and set the attribute value
+                    XmlAttribute att = umbraco.xmlHelper.addAttribute(configDocument, attributeName, attributeValue);
+                    rootNode.Attributes.Append(att);
+                }
+                else
+                {
+                    //Set attribute value
+                    rootNode.Attributes[attributeName].Value = attributeValue;
+                }
+
+                //Save the modified document
+                configDocument.Save(HttpContext.Current.Server.MapPath(configFileName));
+                result = true;
+            }
+            return result;
+        }
+
+        public System.Xml.XmlNode SampleXml()
+        {
+            string sample = "<Action runat=\"install\" undo=\"false\" alias=\"Umbundle.SetAttributeValue\" file=\"~/web.config\" xpath=\"//system.webServer/modules\" attributeName=\"runAllManagedModulesForAllRequests\" attributeValue=\"true\"/>";
+            return umbraco.cms.businesslogic.packager.standardPackageActions.helper.parseStringToXmlNode(sample);
+        }
+
+        /// <summary>
+        /// User RemoveXMLFragment action to uninstall.
+        /// </summary>
+        /// <param name="packageName">Name of the package.</param>
+        /// <param name="xmlData">The XML data.</param>
+        /// <returns></returns>
+        public bool Undo(string packageName, System.Xml.XmlNode xmlData)
+        {
+            return false;
+        }
+    }
 }
