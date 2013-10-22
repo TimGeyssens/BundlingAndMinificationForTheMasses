791152aa3ffb1b2c6a1e21297bf851ded92325b8
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-10-22 11:07:18 +0100

    Add V8JavaScriptEngine Provider (no package actions yet)

--- Optimus.Providers.V8JavaScriptEngine/Properties/AssemblyInfo.cs
+++ Optimus.Providers.V8JavaScriptEngine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Optimus.Providers.Type")]
+[assembly: AssemblyTitle("Optimus.Providers.V8JavaScriptEngine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Optimus.Providers.Type")]
+[assembly: AssemblyProduct("Optimus.Providers.V8JavaScriptEngine")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("083b1ea6-c565-488f-9f53-f3bed8ebbf35")]
+[assembly: Guid("6d89903c-7df3-49e8-a693-6a321e50f87c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
