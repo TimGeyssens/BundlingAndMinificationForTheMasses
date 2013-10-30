69fff90203756893346f0512f7555c3aaaeaf8e3
Tim Geyssens <tg@umbraco.com>
2013-10-30 20:09:57 +0100

    More updates related to v7 compatibility

---
+++ BundlingAndMinificationForTheMasses/Helpers/CompatibilityHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Optimus.Helpers
+{
+    public class CompatibilityHelper
+    {
+
+        public static bool IsVersion7OrNewer
+        {
+            get
+            {
+                var retval = true;
+                try
+                {
+                    typeof (umbraco.uicontrols.CodeArea).InvokeMember("Menu",
+                        BindingFlags.GetField, null, new umbraco.uicontrols.CodeArea(), null);
+                }
+                catch (MissingFieldException e)
+                {
+                    retval = false;
+                }
+
+                return retval;
+            }
+        }
+
+    }
+}
