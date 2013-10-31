0d54589c95dd459aecfbda9eb07be2fa41e1f38e
Tim Geyssens <tg@umbraco.com>
2013-10-31 12:16:58 +0100

    Removes v7 test site

--- TestSiteV7/umbraco/Views/common/dialogs/help.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module("umbraco")
-    .controller("Umbraco.Dialogs.HelpController", function ($scope, $location, $routeParams) {
-        $scope.section = $routeParams.section;
-        if(!$scope.section){
-            $scope.section ="content";
-        }
-    });
--- TestSiteV7/umbraco/Views/propertyeditors/colorpicker/colorpicker.controller.js
+++
@@ -1,7 +0,0 @@
-function ColorPickerController($scope) {
-    $scope.selectItem = function (color) {
-        $scope.model.value = color;
-    };
-}
-
-angular.module("umbraco").controller("Umbraco.PropertyEditors.ColorPickerController", ColorPickerController);
