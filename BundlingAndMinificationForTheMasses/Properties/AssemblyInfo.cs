3c13d421969eee0685e10594f1b27b41a6fefc9f
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-11-04 10:15:20 +0000

    Build Scripts for Optimus.Core

--- BundlingAndMinificationForTheMasses/Properties/AssemblyInfo.cs
+++ BundlingAndMinificationForTheMasses/Properties/AssemblyInfo.cs
@@ -31,6 +31,3 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
--- BundlingAndMinificationForTheMasses/Properties/VersionInfo.cs
+++ BundlingAndMinificationForTheMasses/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.241.27355")]
+[assembly: AssemblyFileVersion("1.2.241.27880")]
 [assembly: AssemblyInformationalVersion("1.2.0-beta")]
 
 
