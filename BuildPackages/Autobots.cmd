9853eac30255acc840f02dc2b0c852a240837dd1
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-11-07 16:29:01 +0000

    Add Autobots.cmd - build all of the packages

---
+++ BuildPackages/Autobots.cmd
@@ -0,0 +1,14 @@
+cd Optimus.Core
+CALL Package.build.cmd
+cd Optimus.Providers.Less
+CALL Package.build.cmd
+cd Optimus.Providers.SassAndScss
+CALL Package.build.cmd
+cd Optimus.Providers.CoffeeScript
+CALL Package.build.cmd
+cd Optimus.Providers.TypeScript
+CALL Package.build.cmd
+cd Optimus.AddOns.V8JavaScriptEngine
+CALL Package.build.cmd
+ECHO The Autobots have been released!
+pause
