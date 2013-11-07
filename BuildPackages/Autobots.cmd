5a157290a6ad8103f9f50de2b98bfe91b23b3198
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-11-07 16:43:11 +0000

    Fix Autobots.cmd

--- BuildPackages/Autobots.cmd
+++ BuildPackages/Autobots.cmd
@@ -1,13 +1,18 @@
 cd Optimus.Core
 CALL Package.build.cmd
+cd..
 cd Optimus.Providers.Less
 CALL Package.build.cmd
+cd..
 cd Optimus.Providers.SassAndScss
 CALL Package.build.cmd
+cd..
 cd Optimus.Providers.CoffeeScript
 CALL Package.build.cmd
+cd..
 cd Optimus.Providers.TypeScript
 CALL Package.build.cmd
+cd..
 cd Optimus.AddOns.V8JavaScriptEngine
 CALL Package.build.cmd
 ECHO The Autobots have been released!
