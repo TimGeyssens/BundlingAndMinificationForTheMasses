cfd70c7b935dd709d3fc810abce465b2c2910706
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-11-15 13:03:45 +0000

    Adding "AddNamespace" package action, so that over install upgrades might be possible one day

--- BundlingAndMinificationForTheMasses/Umbraco/Installer/PackageActions.cs
+++ BundlingAndMinificationForTheMasses/Umbraco/Installer/PackageActions.cs
@@ -14,6 +14,8 @@ namespace Optimus.Umbraco.Installer
     using System.IO;
     using System.Xml.XPath;
 
+    using global::Umbraco.Core.Logging;
+
     using umbraco.cms.businesslogic.packager.standardPackageActions;
 
     public class AddAssemblyBinding : IPackageAction
@@ -846,4 +848,292 @@ namespace Optimus.Umbraco.Installer
 
         #endregion
     }
+
+    public class AddNamespace : IPackageAction
+    {
+        #region IPackageAction AddHiddenSegment
+
+        /// <summary>
+        /// This Alias must be unique and is used as an identifier that must match 
+        /// the alias in the package action XML
+        /// </summary>
+        /// <returns>The Alias in string format</returns>
+        public string Alias()
+        {
+            return "Umbundle.AddNamespace";
+        }
+
+        /// <summary>
+        /// Append the xmlData node to the web.config file
+        /// </summary>
+        /// <param name="packageName">Name of the package that we install</param>
+        /// <param name="xmlData">The data that must be appended to the web.config file</param>
+        /// <returns>True when succeeded</returns>
+        public bool Execute(string packageName, XmlNode xmlData)
+        {
+            //Set result default to false
+            bool result = false;
+
+            //Get attribute values of xmlData
+            string position, nameSpace, file, xPath;
+            position = GetAttributeDefault(xmlData, "position", null);
+            if (!GetAttribute(xmlData, "namespace", out nameSpace) || !GetAttribute(xmlData, "file", out file) || !GetAttribute(xmlData, "xpath", out xPath))
+            {
+                return result;
+            }
+
+            //Create a new xml document
+            XmlDocument document = new XmlDocument();
+
+            //Keep current indentions format
+            document.PreserveWhitespace = true;
+
+            //Load the web.config file into the xml document
+            string configFileName = VirtualPathUtility.ToAbsolute(file);
+            document.Load(HttpContext.Current.Server.MapPath(configFileName));
+
+            //Select root node in the web.config file for insert new nodes
+            XmlNode rootNode = document.SelectSingleNode(xPath);
+
+            //Check for rootNode exists
+            if (rootNode == null) return result;
+
+            //Set modified document default to false
+            bool modified = false;
+
+            //Set insert node default true
+            bool insertNode = true;
+
+            //Check for namespaces node
+            if (rootNode.SelectSingleNode("namespaces") == null)
+            {
+                //Create namespaces node
+                var namespacesNode = document.CreateElement("namespaces");
+                rootNode.AppendChild(namespacesNode);
+
+                //Replace root node
+                rootNode = namespacesNode;
+
+                //Mark document modified
+                modified = true;
+            }
+            else
+            {
+                //Replace root node
+                rootNode = rootNode.SelectSingleNode("namespaces");
+            }
+
+            //Look for existing nodes with same path like the new node
+            if (rootNode.HasChildNodes)
+            {
+                //Look for existing nodeType nodes
+                XmlNode node = rootNode.SelectSingleNode(String.Format("//add[@namespace = '{0}']", nameSpace));
+
+                //If path already exists 
+                if (node != null)
+                {
+                    //Cancel insert node operation
+                    insertNode = false;
+                }
+            }
+
+            //Check for insert flag
+            if (insertNode)
+            {
+                //Create new node with attributes
+                XmlNode newNode = document.CreateElement("add");
+                newNode.Attributes.Append(
+                    XmlHelper.AddAttribute(document, "namespace", nameSpace));
+
+                //Select for new node insert position
+                if (position == null || position == "end")
+                {
+                    //Append new node at the end of root node
+                    rootNode.AppendChild(newNode);
+
+                    //Mark document modified
+                    modified = true;
+                }
+                else if (position == "beginning")
+                {
+                    //Prepend new node at the beginnig of root node
+                    rootNode.PrependChild(newNode);
+
+                    //Mark document modified
+                    modified = true;
+                }
+            }
+
+            //Check for modified document
+            if (modified)
+            {
+                try
+                {
+                    //Save the Rewrite config file with the new rewerite rule
+                    document.Save(HttpContext.Current.Server.MapPath(configFileName));
+
+                    //No errors so the result is true
+                    result = true;
+                }
+                catch (Exception e)
+                {
+                    //Log error message
+                    string message = "Error at execute AddNamespace package action: " + e.Message;
+                    LogHelper.Error(typeof(AddNamespace), message, e);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Removes the xmlData node from the web.config file
+        /// </summary>
+        /// <param name="packageName">Name of the package that we install</param>
+        /// <param name="xmlData">The data that must be appended to the web.config file</param>
+        /// <returns>True when succeeded</returns>
+        public bool Undo(string packageName, System.Xml.XmlNode xmlData)
+        {
+            //Set result default to false
+            bool result = false;
+
+            //Get attribute values of xmlData
+            string nameSpace, file, xPath;
+            if (!GetAttribute(xmlData, "namespace", out nameSpace) || !GetAttribute(xmlData, "file", out file) || !GetAttribute(xmlData, "xpath", out xPath))
+            {
+                return result;
+            }
+
+            //Create a new xml document
+            XmlDocument document = new XmlDocument();
+
+            //Keep current indentions format
+            document.PreserveWhitespace = true;
+
+            //Load the web.config file into the xml document
+            string configFileName = VirtualPathUtility.ToAbsolute(file);
+            document.Load(HttpContext.Current.Server.MapPath(configFileName));
+
+            //Select root node in the web.config file for insert new nodes
+            XmlNode rootNode = document.SelectSingleNode(xPath);
+
+            //Check for rootNode exists
+            if (rootNode == null) return result;
+
+            //Set modified document default to false
+            bool modified = false;
+
+            //Look for existing nodes with same path of undo attribute
+            if (rootNode.HasChildNodes)
+            {
+                //Look for existing add nodes with attribute path
+                foreach (XmlNode existingNode in rootNode.SelectNodes(
+                   String.Format("//add[@namespace = '{0}']", nameSpace)))
+                {
+                    //Remove existing node from root node
+                    rootNode.RemoveChild(existingNode);
+                    modified = true;
+                }
+            }
+
+            if (modified)
+            {
+                try
+                {
+                    //Save the Rewrite config file with the new rewerite rule
+                    document.Save(HttpContext.Current.Server.MapPath(configFileName));
+
+                    //No errors so the result is true
+                    result = true;
+                }
+                catch (Exception e)
+                {
+                    //Log error message
+                    string message = "Error at undo AddNamespace package action: " + e.Message;
+                    LogHelper.Error(typeof(AddNamespace), message, e);
+                }
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Get a named attribute from xmlData root node
+        /// </summary>
+        /// <param name="xmlData">The data that must be appended to the web.config file</param>
+        /// <param name="attribute">The name of the attribute</param>
+        /// <param name="value">returns the attribute value from xmlData</param>
+        /// <returns>True, when attribute value available</returns>
+        private bool GetAttribute(XmlNode xmlData, string attribute, out string value)
+        {
+            //Set result default to false
+            bool result = false;
+
+            //Out params must be assigned
+            value = String.Empty;
+
+            //Search xml attribute
+            XmlAttribute xmlAttribute = xmlData.Attributes[attribute];
+
+            //When xml attribute exists
+            if (xmlAttribute != null)
+            {
+                //Get xml attribute value
+                value = xmlAttribute.Value;
+
+                //Set result successful to true
+                result = true;
+            }
+            else
+            {
+                //Log error message
+                string message = "Error at AddNamespace package action: "
+                     + "Attribute \"" + attribute + "\" not found.";
+                LogHelper.Warn(typeof(AddNamespace), message);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get an optional named attribute from xmlData root node
+        /// when attribute is unavailable, return the default value
+        /// </summary>
+        /// <param name="xmlData">The data that must be appended to the web.config file</param>
+        /// <param name="attribute">The name of the attribute</param>
+        /// <param name="defaultValue">The default value</param>
+        /// <returns>The attribute value or the default value</returns>
+        private string GetAttributeDefault(XmlNode xmlData, string attribute, string defaultValue)
+        {
+            //Set result default value
+            string result = defaultValue;
+
+            //Search xml attribute
+            XmlAttribute xmlAttribute = xmlData.Attributes[attribute];
+
+            //When xml attribute exists
+            if (xmlAttribute != null)
+            {
+                //Get available xml attribute value
+                result = xmlAttribute.Value;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a Sample XML Node 
+        /// In this case we are adding the System.Web.Optimization namespace
+        /// </summary>
+        /// <returns>The sample xml as node</returns>
+        public XmlNode SampleXml()
+        {
+            return helper.parseStringToXmlNode(
+                "<Action runat=\"install\" undo=\"true/false\" alias=\"Umbundle.AddNamespace\" "
+                    + "position=\"beginning/end\" "
+                    + "namespace=\"System.Web.Optimization\""
+                    + "xpath=\"//help\""
+                    + " />");
+        }
+
+        #endregion
+    }
+
 }
