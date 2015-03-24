f66d5c153041249d585b9d48ac4541f37943c65c
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-03-24 16:10:24 +0000

    Fixing a path used in v6 installations for new backoffice routing

--- BundlingAndMinificationForTheMasses/Config.cs
+++ BundlingAndMinificationForTheMasses/Config.cs
@@ -10,7 +10,7 @@ namespace Optimus
         public const string RootDir             = "~/App_Plugins/Optimus/";
         public const string BundlesConfigPath   = RootDir + "config/bundles.config";
         public const string EditPageViewPath    = "~/App_Plugins/Optimus/Views/Index.cshtml";
-        public const string EditPagePath        = "/App_Plugins/Optimus/Index";
+        public const string EditPagePath =      "/umbraco/backoffice/plugins/optimus/index";
         public const string EditFilePagePath    = "/App_Plugins/Optimus/Pages/FileEditor.aspx";
         public const string DialogViewPath      = "~/App_Plugins/Optimus/Views/Dialog.cshtml";
     }
