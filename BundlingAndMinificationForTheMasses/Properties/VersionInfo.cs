f02eebefa4c4555993354defe32e337d542eee92
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-04-13 11:40:07 +0100

    v1.3.2 release

--- BundlingAndMinificationForTheMasses/Properties/VersionInfo.cs
+++ BundlingAndMinificationForTheMasses/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.747.44759")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.762.52827")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
 
 
